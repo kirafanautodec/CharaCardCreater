0c92449d4abbc46375dba25894c6e8c0558a87bf
hhhhhg <2894700792@qq.com>
2018-05-29 04:22:40 +0800

    add title

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ let src,
   fontStyle,
   textContainer;
 let enableScale = false;
-const titleCounts = 24,
+const titleCounts = 25,
   fontsize = 600,
   radius = 800;
 let doubleStart = [0,0,0,0],doubleStartInfo = [0,[0,0]],currentScale = 1;
