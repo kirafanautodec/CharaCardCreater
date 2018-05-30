fecaabe970534e2491b2b45f1b10b3f28245a203
CircleLin <nilcric@outlook.com>
2018-05-30 10:20:27 +0800

    new titles

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ let src,
   fontStyle,
   textContainer;
 let enableScale = false;
-const titleCounts = [14, 6, 4, 11];
+const titleCounts = [14, 6, 10, 12];
 let fontsize = 600, radius = 800;
 let doubleStart = [0,0,0,0],doubleStartInfo = [0,[0,0]],currentScale = 1;
 const dict = [
